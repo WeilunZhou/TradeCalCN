589961a18b92b18ce1a2be38635a308f5d5596a1
WeilunZhou <100591511+WeilunZhou@users.noreply.github.com>
2023-05-15 18:11:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='TradeCalCN',
     version='0.1',
     description='A small Python module for trading days in China stock market',
+    url = 'https://github.com/WeilunZhou/TradeCalCN',
     author='WeilunZhou',
     author_email='3227740026@qq.com',
-    packages=['TradeCalCN'],
+    packages=find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=['baostock>=0.8.8'],
     python_requires='>=3.7'
 )
