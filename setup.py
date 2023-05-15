85288482fa84d6cf070e34eb55fdb9f333e6f56d
WeilunZhou <100591511+WeilunZhou@users.noreply.github.com>
2023-05-15 17:35:50 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='TradeCalCN',
+    version='0.1',
+    description='A small Python module for trading days in China stock market',
+    author='WeilunZhou',
+    author_email='3227740026@qq.com',
+    packages=['TradeCalCN'],
+    install_requires=[],
+)
